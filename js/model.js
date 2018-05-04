fd0bfd9cf476905427b9f7691c09cabab3383a3f
fabianmartinez <fabianemartinez@gmail.com>
2018-05-04 18:31:47 -0500

    Se corrige el problema del reinicio del nivel queda pendiente evaluar un re factor usando un framework

--- js/model.js
+++ js/model.js
@@ -5,8 +5,9 @@ var posX = 0;
 var posY =0;
 var step =0;
 var run =false;
-var totalInactivas = 1;
+var totalInactivas = 2;
 
+const celdInactivas = 2;
 const activa = "#fff"; //casilla activada
 const bloqueada = "#00f"; //casilla en la que no se puede pasar
 const inactiva = "#999"; //casilla que se necesita activar
@@ -14,22 +15,10 @@ const neutral = "#f00"; //casilla que se puede pasar pero no se puede activar
 
 
 const tableroNv1 = [
-	[neutral,neutral,neutral,inactiva,neutral],
-	[neutral,bloqueada,neutral,neutral,neutral],
-	[neutral,neutral,neutral,neutral,neutral]
+	[neutral,bloqueada,bloqueada,bloqueada,inactiva],
+	[neutral,bloqueada,bloqueada,bloqueada,neutral],
+	[neutral,inactiva,neutral,neutral,neutral]
 ];
-
-const tableroNv2 = [
-	[neutral,neutral,neutral,neutral,neutral],
-	[neutral,neutral,neutral,neutral,neutral],
-	[neutral,neutral,neutral,neutral,neutral],
-	[neutral,neutral,neutral,neutral,neutral],
-	[neutral,neutral,neutral,neutral,neutral],
-	[neutral,neutral,neutral,neutral,neutral],
-	[neutral,neutral,neutral,neutral,neutral],
-];
-
-var tablero1 = tableroNv1;
 
 function addAvanzar(){
 	var option = document.createElement("option");
@@ -75,8 +64,8 @@ function cargarnivel(){
 	iniciar.disabled = false;
 	detener.disabled = false;
 	reiniciar.disabled = false;
-	myGamePiece = new component(tablero1, 0, 0);
-	myGameArea.start(tablero1);
+	myGamePiece = new component(tableroNv1, 0, 0);
+	myGameArea.start(tableroNv1);
 }
 
 function ejecutarcomandos(){
@@ -110,7 +99,10 @@ function nextStep(){
 	}else{
 		run = false;
 	}
-	if(totalInactivas == 0){alert("Has ganado");}
+	if(totalInactivas == 0){
+		alert("Has ganado");
+		reiniciarnivel();
+	}
 }
 
 function detenernivel(){
@@ -125,41 +117,10 @@ function reiniciarnivel(){
 	myGamePiece.reiniciarPos();
 	myGamePiece.direccion = "oriente";
 	step=0;
+	totalInactivas = celdInactivas;
 	console.log("oprimi iniciar");
 }
 
-var myGameArea = {
-    start : function(tablero) {
-        this.x = 5;
-		this.y = 5;
-		this.tablero = tablero;
-        context = canvas.getContext("2d");
-		this.interval = setInterval(updateGameArea, 100);
-		for(var i = 0; i < 3; i++){
-			for(var j = 0; j<5; j++){
-				context.fillStyle=this.tablero[i][j];
-				context.fillRect(this.x, this.y, 50, 40);
-				this.x += 60;
-			}
-			this.y += 50;
-			this.x = 5;
-		}
-    },
-    clear : function() {
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		this.x = 5;
-		this.y = 5;
-		for(var i = 0; i < 3; i++){
-			for(var j = 0; j<5; j++){
-				context.fillStyle=this.tablero[i][j];
-				context.fillRect(this.x, this.y, 50, 40);
-				this.x += 60;
-			}
-			this.y += 50;
-			this.x = 5;
-		}
-    }
-}
 
 function updateGameArea() {
 	myGameArea.clear();
@@ -297,8 +258,45 @@ function component(tbl, x, y) {
 		this.direccion = "oriente";
 		this.width = 50;
 		this.length = 20;
-		this.tablero = tableroNv1;
-		this.tablero1 = tableroNv1;
+		for(var j = 0 ; j < this.tablero.length ; j++){
+			for(var k = 0 ; k < this.tablero[0].length ; k++){
+				this.tablero[j][k]=(this.tablero[j][k]==activa)?inactiva:this.tablero[j][k];
+			}
+		}
 	}
+}
+
+
+var myGameArea = {
+    start : function(tablero) {
+        this.x = 5;
+		this.y = 5;
+		this.tablero = tablero;
+        context = canvas.getContext("2d");
+		this.interval = setInterval(updateGameArea, 100);
+		for(var i = 0; i < 3; i++){
+			for(var j = 0; j<5; j++){
+				context.fillStyle=this.tablero[i][j];
+				context.fillRect(this.x, this.y, 50, 40);
+				this.x += 60;
+			}
+			this.y += 50;
+			this.x = 5;
+		}
+    },
+    clear : function() {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		this.x = 5;
+		this.y = 5;
+		for(var i = 0; i < 3; i++){
+			for(var j = 0; j<5; j++){
+				context.fillStyle=this.tablero[i][j];
+				context.fillRect(this.x, this.y, 50, 40);
+				this.x += 60;
+			}
+			this.y += 50;
+			this.x = 5;
+		}
+    }
 }
 
